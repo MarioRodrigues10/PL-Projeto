37f99bad92f9501a7d16c1e065821156ca57f474
Júlio Pinto <pinto.julioj03@gmail.com>
2024-05-08 00:09:25 +0100

    wip: separate by : and ;

--- src/main.py
+++ src/main.py
@@ -3,7 +3,27 @@ import forth_syntax as forth_syntax
 import forth_lexer as forth_lex
 import sys
 
-
+def extract_text(lines):
+    extracted_text = ""
+    list = []
+    bool = False
+    for line in lines:
+        if (line.startswith(":") or bool == True) and not ";" in line:
+            extracted_text += line
+            print(extracted_text)
+            bool = True
+        elif ";" in line and bool == True:
+            extracted_text += line
+            bool = False
+            new_text = extracted_text
+            list.append(new_text)
+            extracted_text = ""
+            
+        else:
+            list.append(line) 
+            
+    return list
+   
 def main():
     forth_l = forth_lex.ForthLexer()
     forthGrammar = forth_grammar.ForthGrammar()
@@ -11,9 +31,12 @@ def main():
     forth_l.build()
 
     lines = sys.stdin.readlines()
+    lines = extract_text(lines)
     for line in lines:
         data = line.strip()
+        print("Data: ", data)
         text = forth_l.test(data)
+        
         # print("Data: ", data)
         # print("Text: ", text)
         result = forthParser.parser.parse(data)
